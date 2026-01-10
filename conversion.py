b4890a4543c4a150c2cd637000a439e43f6de4cd
KBOUAICH0823 <k.bouaich0823@uca.ac.ma>
2026-01-11 00:03:10 +0100

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,19 +1,20 @@
-choix = input("اختار التحويل (1: Celsius → F/K ، 2: Fahrenheit → C): ")
+choix = input("Choisissez la conversion (1: Celsius → F/K, 2: Fahrenheit → C) : ")
 
 try:
     if choix == "1":
-        c = float(input("دخل الدرجة بالـ Celsius: "))
+        c = float(input("Entrez la température en Celsius : "))
         f = c * 9/5 + 32
         k = c + 273.15
         print(f"{c}°C = {f}°F = {k}K")
 
     elif choix == "2":
-        f = float(input("دخل الدرجة بالـ Fahrenheit: "))
+        f = float(input("Entrez la température en Fahrenheit : "))
         c = (f - 32) * 5/9
         print(f"{f}°F = {c}°C")
 
     else:
-        print("اختيار غير صحيح")
+        print("Choix invalide")
 
 except ValueError:
-    print("خطأ: دخل قيمة عددية")
+    print("Erreur : veuillez entrer une valeur numérique")
+
